21eb6ee2630230b39ef2f8209450599289755a8f
Lucas Gomes <lucasagostini400@gmail.com>
2024-08-24 18:43:42 -0300

    Aprendendo sobre os tipos primitivos no Javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+
+    string
+    number
+    boolean
+    null
+    undefined
+
+*/
+
+let nome = 'Lucas'
+let numero = 1
+let usuarioVerificado = true
+let pessoa = null
+let telefone = undefined
+console.log(telefone)
